5d3dac06b5d7005d9e6ac4525cd96b377cf6f8bb
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-09-07 18:19:41 -0300

    add model, dbcontext and migrations

---
+++ ToDoAPI/ToDoAPI/Model/ToDo.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAPI.Model
+{
+    public class ToDo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? ToDoName { get; set; }
+    }
+}
