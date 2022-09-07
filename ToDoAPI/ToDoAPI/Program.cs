b27a51c6515d13d623b6ac4a250e5f42aa8cbb2a
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-09-07 18:20:45 -0300

    add get post put and delete endpoints

--- ToDoAPI/ToDoAPI/Program.cs
+++ ToDoAPI/ToDoAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ToDoAPI.Data;
+using ToDoAPI.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,50 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+
+app.MapGet("api/todo", async (AppDbContext context) =>
+{
+    var items = await context.ToDos.ToListAsync();
+
+    return Results.Ok(items);
+});
+
+app.MapPost("api/todo", async (AppDbContext context, ToDo toDo) =>
+{
+    await context.ToDos.AddAsync(toDo);
+
+    await context.SaveChangesAsync();
+
+    return Results.Created($"api/todo/{toDo.Id}", toDo);
+});
+
+app.MapPut("api/todo/{id}", async (AppDbContext context, int id, ToDo toDo) =>
+{
+    var toDoModel = await context.ToDos.FirstOrDefaultAsync(t => t.Id == id);
+
+    if (toDoModel == null)
+        return Results.NotFound();
+
+    toDoModel.ToDoName = toDo.ToDoName;
+
+    await context.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("api/todo/{id}", async (AppDbContext context, int id) =>
+{
+    var toDoModel = await context.ToDos.FirstOrDefaultAsync(t => t.Id == id);
+
+    if (toDoModel == null)
+        return Results.NotFound();
+
+    context.ToDos.Remove(toDoModel);
+
+    await context.SaveChangesAsync();
+
+    return Results.NoContent();
+});
 
 app.Run();
