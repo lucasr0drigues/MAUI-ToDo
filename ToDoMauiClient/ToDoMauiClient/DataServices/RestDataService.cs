87dc29514d2cfdac28f0734626bb2d4180855953
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-09-08 01:27:30 -0300

    add http client factory

--- ToDoMauiClient/ToDoMauiClient/DataServices/RestDataService.cs
+++ ToDoMauiClient/ToDoMauiClient/DataServices/RestDataService.cs
@@ -11,9 +11,11 @@ namespace ToDoMauiClient.DataServices
         private readonly string _url;
         private readonly JsonSerializerOptions _jsonSerializerOptions;
 
-        public RestDataService()
+        public RestDataService(HttpClient httpClient)
         {
-            _httpClient = new HttpClient();
+            //_httpClient = new HttpClient();
+            _httpClient = httpClient;
+
             _baseAddress = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5279" : "https://localhost:7279";
             _url = $"{_baseAddress}/api";
 
--- ToDoMauiClient/ToDoMauiClient/MauiProgram.cs
+++ ToDoMauiClient/ToDoMauiClient/MauiProgram.cs
@@ -16,9 +16,10 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
-		builder.Services.AddSingleton<IRestDataService, RestDataService>();
+		//builder.Services.AddSingleton<IRestDataService, RestDataService>();
+		builder.Services.AddHttpClient<IRestDataService, RestDataService>();
 
-		builder.Services.AddSingleton<MainPage>();
+        builder.Services.AddSingleton<MainPage>();
 		builder.Services.AddTransient<ManageToDoPage>();
 
 		return builder.Build();
